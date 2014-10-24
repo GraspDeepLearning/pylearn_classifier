86d7e2ad740bfc47ff0f1189a9ad5ba019ef3b16
jvarley <jakevarley@gmail.com>
2014-10-24 11:47:42 -0400

    fixed make_dataset to use the paths set in paths.py rather than hardcoded string

--- make_grasp_dataset_from_patches.py
+++ make_grasp_dataset_from_patches.py
@@ -29,7 +29,6 @@ def preprocess_grasp_dataset(attribs):
     pipeline.items.append(hdf5_data_preprocessors.SplitGraspPatches(
         output_keys=(("train_patches", "train_patch_labels"), ("valid_patches", "valid_patch_labels"), ("test_patches", "test_patch_labels")),
         output_weights=(.8, .1, .1),
-
         source_keys=("normalized_patches", "patch_labels")))
 
     #now we swap around the axis so the data fits nicely onto the gpu
@@ -37,8 +36,8 @@ def preprocess_grasp_dataset(attribs):
     pipeline.items.append(hdf5_data_preprocessors.MakeC01B())
 
     #now lets actually make a new dataset and run it through the pipeline
-    if not os.path.exists(paths.PYLEARN_DATA_PATH + "deep_learning_grasp_data"):
-        os.makedirs(paths.PYLEARN_DATA_PATH + "deep_learning_grasp_data")
+    if not os.path.exists(paths.PROCESSED_TRAINING_DATASET_DIR):
+        os.makedirs(paths.PROCESSED_TRAINING_DATASET_DIR)
 
     hd5f_dataset = h5py.File(attribs["output_filepath"])
     pipeline.apply(hd5f_dataset)
@@ -46,12 +45,12 @@ def preprocess_grasp_dataset(attribs):
 
 if __name__ == "__main__":
 
-    raw_rgbd_datafile = paths.choose_from(paths.RAW_TRAINING_DATASET_DIR)
-    raw_rgbd_filepath = paths.RAW_TRAINING_DATASET_DIR + raw_rgbd_datafile
+    raw_data_filename = paths.choose_from(paths.RAW_TRAINING_DATASET_DIR)
+    raw_data_filepath = paths.RAW_TRAINING_DATASET_DIR + raw_data_filename
 
     preprocess_attribs = dict(sets=("train", "test", "valid"),
                               patch_shape=(170, 170),
-                              raw_filepath=raw_rgbd_filepath,
-                              output_filepath=paths.PROCESSED_TRAINING_DATASET_DIR + 'processed_' + raw_rgbd_datafile)
+                              raw_filepath=raw_data_filepath,
+                              output_filepath=paths.PROCESSED_TRAINING_DATASET_DIR + 'processed_' + raw_data_filename)
 
     preprocess_grasp_dataset(preprocess_attribs)
