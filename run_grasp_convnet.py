a6360a7b65e390339eb3605f97c884369a0fac1f
jvarley <jakevarley@gmail.com>
2014-10-27 12:37:49 -0400

    run_grasp_convnet now hasflag for running garmet classification or grasp classification

--- run_grasp_convnet.py
+++ run_grasp_convnet.py
@@ -4,6 +4,9 @@ from grasp_classification_pipeline import *
 import paths
 import os
 
+RUNNING_GRASPS = True
+
+
 def init_save_file(input_data_file, input_model_file):
 
     dataset_filepath = paths.HEATMAPS_DATASET_DIR + input_data_file[:-3] + '_' + input_model_file + '4.h5'
@@ -24,8 +27,6 @@ def main():
     dataset_file = paths.choose_from(paths.RAW_TRAINING_DATASET_DIR)
     raw_rgbd_filepath = paths.RAW_TRAINING_DATASET_DIR + dataset_file
 
-    #priors_filepath = paths.PRIORS_DIR + 'saxena_rect_priors.h5'
-
     save_filepath = init_save_file(dataset_file, conv_model_name)
 
     pipeline = GraspClassificationPipeline(save_filepath, raw_rgbd_filepath)
@@ -35,10 +36,10 @@ def main():
     pipeline.add_stage(FeatureExtraction(conv_model_filepath, use_float_64=False))
     pipeline.add_stage(Classification(conv_model_filepath))
     pipeline.add_stage(HeatmapNormalization())
-    #pipeline.add_stage(ConvolvePriors(priors_filepath))
-    #pipeline.add_stage(CalculateMax())
-    #pipeline.add_stage(CalculateTopFive(input_key='convolved_heatmaps', output_key='dependent_grasp_points'))
-    #pipeline.add_stage(CalculateTopFive(input_key='normalized_heatmaps', output_key='independent_grasp_points'))
+
+    if RUNNING_GRASPS:
+        priors_filepath = paths.PRIORS_DIR + 'saxena_rect_priors.h5'
+        pipeline.add_stage(ConvolvePriors(priors_filepath))
 
     pipeline.run()
 
