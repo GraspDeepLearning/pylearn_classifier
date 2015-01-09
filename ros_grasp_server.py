ad715766eeaaa3c3ab0f15c9b183394a05fbaad8
Jake Varley <jakevarley@gmail.com>
2015-01-09 14:57:42 -0500

    merginb

--- ros_grasp_server.py
+++ ros_grasp_server.py
@@ -10,7 +10,7 @@ from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
 
 from uvd_xyz_conversion.srv import UVDTOXYZ
-from pylearn_classifier_gdl.srv import CalculateGraspsService
+from pylearn_classifier_gdl.srv import CalculateGraspsService, CalculateGraspsServiceResponse
 from gdl_grasp_msgs.msg import Grasp
 
 import tf
@@ -53,8 +53,13 @@ class GraspServer:
     def __init__(self):
         self.pylearn_model = None
 
-
         conv_model_name = "processed_just_tobasco_with_wrist_roll_5_layer_170x170_12_11_11_55"
+
+        try:
+            rospy.wait_for_service('/uvd_to_xyz', 6)
+        except rospy.ServiceException as exc:
+            rospy.logerr("uvd_to_xyz service never came up")
+            assert False
 
         self.uvd_to_xyz_proxy = rospy.ServiceProxy('uvd_to_xyz', UVDTOXYZ)
 
@@ -75,6 +80,7 @@ class GraspServer:
 
         rospy.logout(raw_rgbd_filepath)
         rospy.logout(save_filepath)
+
         self.pipeline = BarrettGraspClassificationPipeline(save_filepath, raw_rgbd_filepath, conv_model_filepath, input_key="rgbd")
         self.pipeline.run()
 
@@ -84,49 +90,54 @@ class GraspServer:
 
     def service_request_handler(self, request):
         rospy.loginfo("received request")
+
         rgbd = np.array(request.rgbd).reshape((480, 640, 4))
         mask = np.array(request.mask).reshape((480, 640))
 
         self.input_dset["rgbd"][0] = rgbd
-        self.save_dset["mask"] = mask
+        self.save_dset["mask"][:, :] = mask
 
         self.pipeline.run()
 
         grasps = self.pipeline._pipeline_stages[-1].grasps
 
-        grasp_msgs = []
+        grasp_msgs = CalculateGraspsServiceResponse()
 
         for grasp in grasps:
             grasp_energy, grasp_type,  palm_index, argmax_u, argmax_v = grasp
             d = rgbd[argmax_u, argmax_v, 3]
-            x, y, z = self.uvd_to_xyz_proxy(argmax_u, argmax_v, d)
 
-            joint_values = self.mean_grasps.joint_values[grasp_type]
-            wrist_roll = self.mean_grasps.wrist_roll[grasp_type]
+            resp = self.uvd_to_xyz_proxy(argmax_u, argmax_v, d)
+            x = resp.x
+            y = resp.y
+            z = resp.z
 
-            quat = tf.transformations.quaternion_from_euler((0, 0, wrist_roll))
+            joint_values = self.grasp_priors_list.get_grasp_prior(grasp_type).joint_values
+            wrist_roll = self.grasp_priors_list.get_grasp_prior(grasp_type).wrist_roll
+
+            quat = tf.transformations.quaternion_from_euler(0, 0, wrist_roll)
 
             grasp_msg = Grasp()
             grasp_msg.pose.position.x = x
             grasp_msg.pose.position.y = y
             grasp_msg.pose.position.z = z
-            grasp_msg.pose.orientation.x = quat.x
-            grasp_msg.pose.orientation.y = quat.y
-            grasp_msg.pose.orientation.z = quat.z
-            grasp_msg.pose.orientation.w = quat.w
+            grasp_msg.pose.orientation.x = quat[0]
+            grasp_msg.pose.orientation.y = quat[1]
+            grasp_msg.pose.orientation.z = quat[2]
+            grasp_msg.pose.orientation.w = quat[3]
 
             grasp_msg.joint_values = joint_values
-            grasp_msg.grasp_enery = grasp_energy
+            grasp_msg.grasp_energy = grasp_energy
             grasp_msg.grasp_type = grasp_type
 
-            grasp_msgs.append(grasp_msg)
+            grasp_msgs.grasps.append(grasp_msg)
+
+        rospy.loginfo("finished")
 
         return grasp_msgs
-
-
 
 if __name__ == "__main__":
 
     rospy.init_node('grasp_server_node')
-    rospy.loginfo("starting grasp_server_node!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    rospy.loginfo("starting grasp_server_node...")
     grasp_server = GraspServer()
